2dc85fcda8914c3f185024114e6ea5c5d5db49c9
xuanhien070594@gmail.com <xuanhien070594@gmail.com>
2022-03-23 23:06:19 -0400

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="trifinger_mujoco",
+    version="0.0.1",
+    install_requires=["gym"],  # Add any other dependencies here
+    python_requires=">=3",
+    description="trifinger-mujoco: A gym-compatible environment of Trifinger robot for contact-rich manipulation tasks",
+    author="Hien Bui",
+    author_email="xuanhien@seas.upenn.edu",
+    url="https://github.com/xuanhien070594/trifinger-mujoco",
+    license="MIT",
+)
