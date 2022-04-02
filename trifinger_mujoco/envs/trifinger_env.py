089b62ce560baa201da49b92c99191bd786d3221
xuanhien070594@gmail.com <xuanhien070594@gmail.com>
2022-04-02 10:23:47 -0400

    add pro version of trifinger robot

--- trifinger_mujoco/envs/trifinger_env.py
+++ trifinger_mujoco/envs/trifinger_env.py
@@ -5,6 +5,18 @@ from gym import utils
 from gym.envs.mujoco import mujoco_env
 import trifinger_mujoco.utils as tf_utils
 
+# robot and controller type
+XML_LIBRARY = {
+    "edu": {
+        "position": "trifinger_with_cube_pos_ctrl.xml",
+        "torque": "trifinger_with_cube_torque_ctrl.xml",
+    },
+    "pro": {
+        "position": "trifinger_pro_with_cube_pos_ctrl.xml",
+        "torque": "trifinger_pro_with_cube_torque_ctrl.xml",
+    },
+}
+
 
 class TrifingerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self, env_configs: Dict[str, Any]):
@@ -14,11 +26,12 @@ class TrifingerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
           env_configs: contains all necessary configurations for the environment
 
         """
-        model_path = path.join(
-            path.dirname(__file__), "../models/trifinger_with_cube.xml"
-        )
         # obtain config parameters
         self.env_configs = env_configs
+
+        self.robot_type = self.env_configs["robot_type"]
+        self.controller_type = self.env_configs["controller_type"]
+        self.model_name = XML_LIBRARY[self.robot_type][self.controller_type]
 
         # initial positions and velocities for each finger
         self.init_finger_pos = np.array(env_configs["init_finger_pos"])
@@ -32,46 +45,13 @@ class TrifingerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
             self.init_cube_axis_angle[:3], self.init_cube_axis_angle[-1]
         )
 
-        self.max_torque = env_configs["max_torque"]
+        self.max_delta_position = env_configs["max_delta_position"]
         self.use_contact_forces = env_configs["use_contact_forces"]
         self.use_random_target = env_configs["use_random_target"]
 
-        # target positions and orienetations of the cube
-        if self.use_random_target:
-            self.random_target_cube_pos_low = np.array(
-                env_configs["random_target_cube_pos_low"]
-            )
-            self.random_target_cube_pos_high = np.array(
-                env_configs["random_target_cube_pos_high"]
-            )
+        self._sample_target()
 
-            self.target_cube_pos = np.random.uniform(
-                self.random_target_cube_pos_low, self.random_target_cube_pos_high
-            )
-
-            while (
-                abs(self.target_cube_pos[0]) < 0.05
-                or abs(self.target_cube_pos[1]) < 0.05
-            ):
-                self.target_cube_pos = np.random.uniform(
-                    self.random_target_cube_pos_low, self.random_target_cube_pos_high
-                )
-            self.target_cube_quat = tf_utils.quaternion.random()
-        else:
-            self.target_cube_pos = env_configs["target_cube_pos"]
-            self.target_cube_axis_angle = env_configs["target_cube_axis_angle"]
-            self.target_cube_quat = tf_utils.axis_angle.to_quaternion(
-                self.target_cube_axis_angle[:3], self.target_cube_axis_angle[-1]
-            )
-
-        self.init_cube_pos_error = np.linalg.norm(
-            self.target_cube_pos - self.init_cube_pos, ord=2
-        )
-
-        self.init_cube_quat_error = tf_utils.quaternion.quat_error(
-            self.target_cube_quat, self.init_cube_quat
-        )
-
+        model_path = path.join(path.dirname(__file__), f"../models/{self.model_name}")
         mujoco_env.MujocoEnv.__init__(self, model_path, 20)
         utils.EzPickle.__init__(self)
 
@@ -100,8 +80,9 @@ class TrifingerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
           action: Shape(9, ). Desired joint torque
 
         """
-        action = np.clip(action, -1, 1) * self.max_torque
-        self.do_simulation(action, self.frame_skip)
+        action = np.clip(action, -1, 1) * self.max_delta_position
+        desired_joint_position = action + self.sim.data.qpos[7:16]
+        self.do_simulation(desired_joint_position, self.frame_skip)
         obs = self._get_obs()
         done = self._is_done(obs)
         reward = self._reward(obs, action)
@@ -109,16 +90,110 @@ class TrifingerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
     def _reward(self, state: np.ndarray, action: np.ndarray):
         """Define reward function."""
-        cur_cube_pos = state[:3]
-        cur_cube_quat = state[3:7]
+        cube_xy_cost = self._compute_cube_xy_cost(state)
+        cube_z_cost = self._compute_cube_z_cost(state)
+        cube_quat_cost = self._compute_cube_quat_cost(state)
+        cube_finger_tip_cost = self._compute_cube_finger_dist_cost(state)
+
+        total_cost = (
+            0.25 * cube_xy_cost
+            + 0.5 * cube_z_cost
+            + 0.1 * cube_quat_cost
+            + 0.15 * cube_finger_tip_cost
+        )
+
+        return -total_cost
+
+    def _compute_cube_quat_cost(self, state: np.ndarray) -> float:
+        """Return the cost based on the relative quaternion error between the cube and the target."""
+        return (
+            tf_utils.quaternion.quat_error(self.target_cube_quat, state[3:7])
+            / self.init_cube_quat_error
+        )
+
+    def _compute_cube_xy_cost(self, state: np.ndarray) -> float:
+        """Return the cost based on the relative horizontal position between the cube and the target."""
+        return (
+            np.linalg.norm(state[:2] - self.target_cube_pos[:2], ord=2)
+            / self.init_cube_xy_error
+        )
+
+    def _compute_cube_z_cost(self, state: np.ndarray) -> float:
+        """Return the cost based on the relative vertical position between the cube and the target."""
+        return abs(state[2] - self.target_cube_pos[2]) / self.init_cube_z_error
+
+    def _compute_cube_finger_dist_cost(self, state: np.ndarray) -> float:
+        """Return the cost based on the distance between the finger tips and the cube."""
+        finger_lower_link_0_id = self.sim.model.body_name2id("finger_lower_link_0")
+        finger_lower_link_120_id = self.sim.model.body_name2id("finger_lower_link_120")
+        finger_lower_link_240_id = self.sim.model.body_name2id("finger_lower_link_240")
+
+        finger_lower_link_0_pos = self.sim.data.body_xpos[finger_lower_link_0_id]
+        finger_lower_link_120_pos = self.sim.data.body_xpos[finger_lower_link_120_id]
+        finger_lower_link_240_pos = self.sim.data.body_xpos[finger_lower_link_240_id]
+
+        finger_tip_0_pos = finger_lower_link_0_pos + np.array([0.0, 0.0, -0.16])
+        finger_tip_120_pos = finger_lower_link_0_pos + np.array([0.0, 0.0, -0.16])
+        finger_tip_240_pos = finger_lower_link_0_pos + np.array([0.0, 0.0, -0.16])
+
+        dist_finger_tip_0_cube = np.linalg.norm(finger_tip_0_pos - state[:3], ord=2)
+        dist_finger_tip_120_cube = np.linalg.norm(finger_tip_120_pos - state[:3], ord=2)
+        dist_finger_tip_240_cube = np.linalg.norm(finger_tip_240_pos - state[:3], ord=2)
+
+        normalized_dist_finger_tip_0_cube = (dist_finger_tip_0_cube - 0.025) / (
+            0.15 - 0.025
+        )
+        normalized_dist_finger_tip_120_cube = (dist_finger_tip_120_cube - 0.025) / (
+            0.15 - 0.025
+        )
+        normalized_dist_finger_tip_240_cube = (dist_finger_tip_240_cube - 0.025) / (
+            0.15 - 0.025
+        )
 
         return (
-            2
-            - np.linalg.norm(cur_cube_pos - self.target_cube_pos, ord=2)
-            / self.init_cube_pos_error
-            - tf_utils.quaternion.quat_error(self.target_cube_quat, cur_cube_quat)
-            / self.init_cube_quat_error
-        ) / 2.0
+            normalized_dist_finger_tip_0_cube
+            + normalized_dist_finger_tip_120_cube
+            + normalized_dist_finger_tip_240_cube
+        ) / 3.0
+
+    def _sample_target(self):
+        """Randomly select the target position and orientation of the cube."""
+        if self.use_random_target:
+            self.random_target_cube_pos_low = np.array(
+                self.env_configs["random_target_cube_pos_low"]
+            )
+            self.random_target_cube_pos_high = np.array(
+                self.env_configs["random_target_cube_pos_high"]
+            )
+
+            self.target_cube_pos = np.random.uniform(
+                self.random_target_cube_pos_low, self.random_target_cube_pos_high
+            )
+
+            while (
+                abs(self.target_cube_pos[0]) < 0.05
+                or abs(self.target_cube_pos[1]) < 0.05
+            ):
+                self.target_cube_pos = np.random.uniform(
+                    self.random_target_cube_pos_low, self.random_target_cube_pos_high
+                )
+            self.target_cube_quat = tf_utils.quaternion.random()
+        else:
+            self.target_cube_pos = self.env_configs["target_cube_pos"]
+            self.target_cube_axis_angle = self.env_configs["target_cube_axis_angle"]
+            self.target_cube_quat = tf_utils.axis_angle.to_quaternion(
+                self.target_cube_axis_angle[:3], self.target_cube_axis_angle[-1]
+            )
+
+        self.init_cube_xy_error = np.linalg.norm(
+            self.target_cube_pos[:2] - self.init_cube_pos[:2], ord=2
+        )
+
+        self.init_cube_z_error = abs(self.target_cube_pos[2] - self.init_cube_pos[2])
+
+        self.init_cube_quat_error = tf_utils.quaternion.quat_error(
+            self.target_cube_quat, self.init_cube_quat
+        )
 
     def _is_done(self, state: np.ndarray) -> bool:
         """Check if terminating conditions are met, given the current state."""
@@ -161,11 +236,12 @@ class TrifingerEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def reset_model(self) -> np.ndarray:
         """Set initial conditions for the environment."""
         self.set_state(self.init_qpos.copy(), self.init_qvel.copy())
+        self._sample_target()
 
         # set target positions and orienetations (visualization only)
-        target_id = self.sim.model.geom_name2id("target")
-        self.sim.model.geom_pos[target_id] = self.target_cube_pos
-        self.sim.model.geom_quat[target_id] = self.target_cube_quat
+        target_id = self.sim.model.body_name2id("target")
+        self.sim.model.body_pos[target_id] = self.target_cube_pos
+        self.sim.model.body_quat[target_id] = self.target_cube_quat
 
         return self._get_obs()
 
--- trifinger_mujoco/scripts/test_trifinger_env.py
+++ trifinger_mujoco/scripts/test_trifinger_env.py
@@ -1,10 +1,16 @@
 import trifinger_mujoco
 import numpy as np
 import gym
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
 
 if __name__ == "__main__":
     env = gym.make("TrifingerForces-v0")
     env.reset()
-    for i in range(100000):
-        obs, reward, done, _ = env.step(np.zeros(9))
+    # video = VideoRecorder(env, "trifinger.mp4")
+    print(env.action_space)
+
+    for i in range(1000):
+        obs, reward, done, _ = env.step(np.ones(9))
+        # video.capture_frame()
         env.render()
+    # video.close()
